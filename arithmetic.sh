529f0a62ad0499392898274e3af73100a7f9fc8a
sushant kiran <sushantpedhapalli@gmail.com>
2020-09-05 18:09:08 +0530

    shell scripts

---
+++ arithmetic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+num1=17
+num2=10
+
+echo "$(expr $num1 + $num2 )"
+echo "$(expr $num1 - $num2 )"
+echo "$(expr $num1 \* $num2 )"
+echo "$(expr $num1 / $num2 )"
+echo "$(expr $num1 % $num2 )"
+
+echo 
+echo "$(( num1 + num2 ))"
+echo "$(( num1 - num2 ))"
+echo "$(( num1 * num2 ))"
+echo "$(( num1 / num2 ))"
+echo "$(( num1 % num2 ))"
---
+++ float.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+a=10.5
+b=5.5
+
+echo "$a+$b" | bc
