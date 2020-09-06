0b4a052ad6cfc30b1ecea7229ec74a9ee5cbb38c
sushant <sushantpedhapalli@gmail.com>
2020-09-06 00:11:56 -0700

    array

---
+++ array.sh
@@ -0,0 +1,10 @@
+#! /bin/bash 
+ 
+# To declare static Array  
+arr=(prakhar ankit 1 rishabh manish abhinav) 
+ 
+# To print all elements of array 
+echo ${arr[@]}        
+echo ${!arr[*]}        
+echo ${#arr[@]}     
+echo ${arr[2]} 
